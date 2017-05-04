74f16167cea0336c5572057954e016c40e28cff6
Alex Stubbs <alex.stubbs@100Starlings.com>
2017-05-04 12:00:23 +0100

    Changing migrations to Rails 5 syntax

--- lib/generators/joyce/templates/migration.rb
+++ lib/generators/joyce/templates/migration.rb
@@ -7,7 +7,7 @@ class AddJoyceTables < ActiveRecord::Migration
       t.timestamps
     end
     add_index(:joyce_activities, :created_at)
-    
+
     create_table 'joyce_streams', :force => true do |t|
       t.string      'name'
       t.integer     :owner_id
@@ -16,28 +16,25 @@ class AddJoyceTables < ActiveRecord::Migration
     end
     add_index(:joyce_streams, :owner_type)
     add_index(:joyce_streams, [:owner_id, :owner_type])
-    
+
     create_table 'joyce_activities_streams', :id => false, :force => true do |t|
       t.references  :activity, :null => false
       t.references  :stream, :null => false
     end
-    add_index(:joyce_activities_streams, :activity_id)
-    add_index(:joyce_activities_streams, :stream_id)
-    
+
     create_table 'joyce_activities_targets', :id => false, :force => true do |t|
       t.string      'name'
       t.references  :activity, :null => false
       t.references  :target, :polymorphic => true, :null => false
     end
-    
+
     create_table 'joyce_streams_subscribers', :force => true do |t|
-      t.references  :subscriber, :polymorphic => true, :null => false
+      t.references  :subscriber, :polymorphic => true, :null => false,
+        :index => { :name => "index_joyce_streams_subscribers_on_subscriber" }
       t.references  :stream, :null => false
       t.datetime    'started_at', :null => false
       t.datetime    'ended_at'
     end
-    add_index(:joyce_streams_subscribers, [:subscriber_id, :subscriber_type], :name => "index_joyce_streams_subscribers_on_subscriber")
-    add_index(:joyce_streams_subscribers, :stream_id)
   end
 
   def self.down
